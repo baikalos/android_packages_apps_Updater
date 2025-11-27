a51e2997f42f37232a7801ac7670881377eec943
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:07:18 +0300

    Baikalify 1
    Change-Id: I3b6f9a1c6b05076cba9919f82b6dfc16bb91e16b

--- app/src/main/java/org/lineageos/updater/misc/Constants.java
+++ app/src/main/java/org/lineageos/updater/misc/Constants.java
@@ -30,10 +30,10 @@ public final class Constants {
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_ALLOW_MAJOR_UPGRADES = "lineage.updater.allow_major_upgrades";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.crdroid.build.version";
-    public static final String PROP_DEVICE = "ro.crdroid.device";
+    public static final String PROP_BUILD_VERSION = "ro.baikalos.build.version";
+    public static final String PROP_DEVICE = "ro.baikalos.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "crdroid.updater.allow_downgrading";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "baikalos.updater.allow_downgrading";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
