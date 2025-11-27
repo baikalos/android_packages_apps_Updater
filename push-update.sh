72f1cd71d444884e791f8a9822d98cb3b42cf6ec
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:07:18 +0300

    Baikalify 1
    Change-Id: I3b6f9a1c6b05076cba9919f82b6dfc16bb91e16b

--- push-update.sh
+++ push-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-updates_dir=/data/crdroid_updates
+updates_dir=/data/baikalos_updates
 
 # $1 = ZIP
 # $2 = UNVERIFIED (optional)
@@ -9,7 +9,7 @@ if [ ! -f "$1" ]; then
    echo "Usage: $0 ZIP [UNVERIFIED] [SERIAL]"
    echo "Push ZIP to $updates_dir and add it to Updater"
    echo
-   echo "The name of ZIP is assumed to have crdroid-VERSION-DATE-TYPE-* as format"
+   echo "The name of ZIP is assumed to have BaikalOS-VERSION-DEVICE-TYPE-DATE-* as format"
    echo "If UNVERIFIED is set, the app will verify the update"
    exit
 fi
@@ -41,12 +41,12 @@ else
     status=2
 fi
 
-# Assume crdroid-VERSION-DATE-TYPE-*.zip
+# Assume BaikalOS-VERSION-DEVICE-TYPE-DATE-*.zip
 zip_name=`basename "$zip_path"`
 id=`echo "$zip_name" | sha1sum | cut -d' ' -f1`
 version=`echo "$zip_name" | cut -d'-' -f2`
 type=`echo "$zip_name" | cut -d'-' -f4`
-build_date=`echo "$zip_name" | cut -d'-' -f3 | cut -d'_' -f1`
+build_date=`echo "$zip_name" | cut -d'-' -f5 | cut -d'_' -f1`
 if [ "`uname`" = "Darwin" ]; then
     timestamp=`date -jf "%Y%m%d %H:%M:%S" "$build_date 23:59:59" +%s`
     size=`stat -f%z "$zip_path"`
